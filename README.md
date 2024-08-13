ba46d33278ec89b6a17db1ff3b236e4d9884109d
JpCardenasG <jpcardenasgonzalez@gmail.com>
2024-08-13 09:58:25 -0500

    Readme added

---
+++ README.md
@@ -0,0 +1,60 @@
+# Proyecto de CRUD para la Entidad Customer en Spring Boot
+
+Este proyecto es una aplicación Spring Boot que implementa un CRUD básico para la entidad Customer.
+
+Entidad Seleccionada
+
+    Tabla: Customer
+
+Endpoints Disponibles
+1. Crear un Cliente
+
+    URL: /api/customer/saveCustomer
+    Método HTTP: POST
+    Descripción: Crea un nuevo registro de cliente en la base de datos.
+    Cuerpo de la Solicitud:
+
+    ```json
+
+    {
+        "id": "string",
+        "namecustomer": "string",
+        "lastnamecustomer": "string",
+        "emailcustomer": "string",
+        "longcustomer": "string",
+        "latcustomer": "string",
+        "birthdate": "string",
+        "city" : {
+            "id": "long",
+            "name": "string"
+        }
+        
+    }
+    ```
+
+    Respuesta: El cliente creado.
+
+2. Actualizar un Cliente
+
+    URL: /api/customer/updateCustomer/{customerId}
+    Método HTTP: PUT
+    Descripción: Actualiza un registro existente de cliente en la base de datos.
+    Parámetros de Ruta:
+        customerId: ID del cliente a actualizar.
+
+    Respuesta: El cliente actualizado.
+
+3. Eliminar un Cliente
+
+    URL: /api/customer/deleteCustomer/{customerId}
+    Método HTTP: DELETE
+    Descripción: Elimina un registro de cliente en la base de datos.
+    Parámetros de Ruta:
+        customerId: ID del cliente a eliminar.
+    Respuesta: ID del cliente eliminado.
+
+4. Obtener Todos los Clientes
+
+    URL: /api/customer/allCustomers
+    Método HTTP: GET
+    Descripción: Recupera todos los registros de clientes en la base de datos.
---
+++ src/main/java/com/campuslands/farmacy/infrastructure/in/CityController.java
@@ -0,0 +1,49 @@
+package com.campuslands.farmacy.infrastructure.in;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.campuslands.farmacy.application.service.CityService;
+import com.campuslands.farmacy.domain.models.City;
+
+@RestController
+@RequestMapping("/api/city")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @PostMapping("/saveCity")
+    public ResponseEntity<City> saveCity(@RequestBody City city) {
+        service.saveCity(city);
+        return ResponseEntity.ok(city);
+    }
+
+    @PutMapping("/updateCity/{cityId}")
+    public ResponseEntity<City> updateCity(@PathVariable Long cityId, @RequestBody City city) {
+        service.updateCity(cityId, city);
+        return ResponseEntity.ok(city);
+    }
+
+    @DeleteMapping("/deleteCity/{cityId}")
+    public ResponseEntity<Long> deleteCity(@PathVariable Long cityId) {
+        service.deleteCity(cityId);
+        return ResponseEntity.ok(cityId);
+    }
+
+    @GetMapping("/allCities")
+    public ResponseEntity<List<City>> findAll() {
+        return ResponseEntity.ok(service.fetchCitiesList());
+    }
+}
